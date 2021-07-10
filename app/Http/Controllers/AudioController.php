debd042443df8340439ae23dc0695f0661d99e58
Asi Zaltzman <asi.zaltzman@imnasol.com>
2021-07-10 20:13:09 +0300

    refactor get url

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -18,32 +18,35 @@ class AudioController extends ApiController
 
     public function promo2mp3(Request $request){
 
-//        $guzzleClient = new GuzzleClient(['timeout' => 60]);
-        $goutteClient = new Client();
+        $promo_template = $request->get('promo_template');
 
-//        $goutteClient->setClient($goutteClient);
-        $crawler = $goutteClient->request('GET', 'https://promo.com/templates/spring');
+        //query db if url is there and file is there.
+
+        $goutteClient = new Client();
+        $crawler = $goutteClient->request(
+            'GET', env('PROMO_URL','https://promo.com/templates/').'/'.$promo_template
+        );
 
         $first_image = $crawler->filter('.video-item__thumbnail')->attr('data-src');
-
-        $mp4_url = str_replace(array('collections', '--thumb-small.jpg?dv=4'),
-            array('promoVideos', '/preview.mp4?dv=4'), $first_image);
 
         $uri_parts = explode('/', $first_image);
         $end_uri = end($uri_parts);
         $id = substr($end_uri, 0, strpos($end_uri, '-'));
 
+        $mp4_url = str_replace(array('collections', '--thumb-small.jpg?dv=4'),
+            array('promoVideos', '/preview.mp4?dv=4'), $first_image);
+
 
         try{
-//        FFMpeg::openUrl($mp4_url)->export()->inFormat(new \FFMpeg\Format\Audio\Mp3)->save('test.mp3');
-//        FFMpeg::open($mp4_url)->export()->addFormatOutputMapping(new \FFMpeg\Format\Audio\Mp3, Media::make('local', 'test.mp3'), array('0:a'))->save();
             $ffmpeg = FFMpeg::create(array(
                 'ffmpeg.binaries'  => '/usr/bin/ffmpeg',
                 'ffprobe.binaries' => '/usr/bin/ffprobe',
                 'timeout'          => 3600, // The timeout for the underlying process
                 'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
             ));
-            $advancedMedia = $ffmpeg->openAdvanced(array('https://ak02-video-cdn.slidely.com/promoVideos/videos/5e/3a/5e3a8a2b91fbb372d44236b3/preview.mp4?dv=4'));
+            $advancedMedia = $ffmpeg->openAdvanced(
+                array('https://ak02-video-cdn.slidely.com/promoVideos/videos/5e/3a/5e3a8a2b91fbb372d44236b3/preview.mp4?dv=4')
+            );
             $advancedMedia->map(array('0:a'),  new Mp3(),storage_path('output.mp3') )
                 ->save();
     }catch (\Exception $e){
